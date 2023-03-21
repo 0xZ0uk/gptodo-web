c8be8d2628fec0060dc616b6d6e45818f2fb24e2
Pedro Santana <pedrosantana@duck.com>
2023-03-21 23:42:35 +0000

    Last commit missing touches

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,9 +18,9 @@ const Layout: React.FC<LayoutProps> = ({ name, children, footer }) => {
       </Head>
       <main
         id={name}
-        className="flex h-screen w-screen justify-center overflow-hidden bg-black"
+        className="flex h-screen max-h-screen min-h-screen w-screen justify-center overflow-hidden bg-black"
       >
-        <div className="m-4 flex w-full flex-col rounded-md bg-white p-4">
+        <div className="m-4 flex w-full flex-col rounded-md bg-white px-4 py-2">
           <Header />
           {children}
           {footer}
